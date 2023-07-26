681beea8b1f68f8430a8f27ba3748f119c9ada36
fmu17 <fmu17@yandex.ru>
2023-07-26 22:11:46 +0300

    hello world

--- Exercise_1.cs
+++ Exercise_1.cs
@@ -1,1 +1,10 @@
-
+// код программы «Привет, мир!»
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Привет, мир!");
+        }
+    }
+}
