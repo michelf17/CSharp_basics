adc10368cb46f094a15a0c2f3285a202f161fcdb
fmu17 <fmu17@yandex.ru>
2023-07-26 22:54:03 +0300

    program for square calculation - second version

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
+// Mathematical method
 int Square = number * number;
-// Console.WriteLine(Square);
-Console.WriteLine($"Квадрат числа равен {Square}");
+Console.WriteLine($"Квадрат числа {number} равен {Square}");
+
+// Library method
+int square2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен {square2}");
