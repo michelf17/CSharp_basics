1a1a6c9441bb128c1f54cb17d70a9448d26dbae2
fmu17 <fmu17@yandex.ru>
2023-07-26 22:35:49 +0300

    Delete unused files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program for calculation square of a given number.
+//
+
+Console.WriteLine("Введите число: ");
