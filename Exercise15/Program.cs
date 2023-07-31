2d0d7ec50607cd8b5c8569751de3b7ab28a99e9d
fmu17 <fmu17@yandex.ru>
2023-07-31 18:43:58 +0300

    Exercise 15

---
+++ Exercise15/Program.cs
@@ -0,0 +1,31 @@
+﻿//---------------------------------------------------------------------------------------------------
+//
+// Exercise 15. Program for input week's day number and output if it is at weekend. Input must be in range 1 - 7, where 1 - monday, 7 - sunday.
+// Test data:
+// 6 -> 'да'
+// 7 -> 'да'
+// 1 -> 'нет'
+//
+bool isCorrect = false;
+int day = 0;
+int lowlimit = 1;
+int hilimit = 7;
+do
+{
+    Console.Write("Введите индекс дня недели: ");
+    day = int.Parse(Console.ReadLine());
+    isCorrect = true;
+    if (day < lowlimit) isCorrect = false;
+    if (day > hilimit) isCorrect = false;
+    if (isCorrect == false) {
+        Console.WriteLine($"Повторите ввод. Номер дня недели должен быть в диапазоне {lowlimit} - {hilimit}");
+    }
+} while (isCorrect == false); 
+//
+string isWeekend = "";
+if (day < 6) isWeekend = "нет";
+else isWeekend = "да";
+// Output result string
+Console.WriteLine($"День недели №{day} - это выходной?");
+Console.WriteLine("{0}", isWeekend);
+//
